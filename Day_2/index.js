913f8e953089afb5fa66c208db1cd444add45095
lamidimorufat <lamidimorufat0@gmail.com>
2023-12-12 23:50:49 +0100

    done with day 2 level 1 exercises

---
+++ Day_2/index.js
@@ -0,0 +1,88 @@
+// Exercise Level 1
+
+// solution 1
+let challenge = "30 Days Of Javascript"
+
+// solution 2
+console.log(challenge)
+
+// solution 3
+console.log(challenge.length)
+
+// solution 4
+console.log(challenge.toUpperCase())
+
+// solution 5
+console.log(challenge.toLowerCase())
+
+// solution 6
+console.log(challenge.substring(0, 2))
+
+// solution7
+console.log(challenge.substring(3))
+
+// solution 8
+console.log(challenge.includes("Script"))
+
+// solution 9
+console.log(challenge.split())
+
+// solution 10
+console.log(challenge.split(""))
+
+// solution 11
+let brands = "facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(brands.split(","))
+
+// solution 12
+console.log(challenge.replace("Javascript","Python"))
+
+// Solution 13
+console.log(challenge.charAt(15))
+
+// solution 14
+console.log(challenge.charCodeAt(11))
+
+// solution 15
+console.log(challenge.indexOf("a"))
+
+// solution 16
+console.log(challenge.lastIndexOf("a"))
+
+// solution 17
+let soluTion = "You cannot end a sentence with because because because is a conjunction"
+console.log(soluTion.indexOf("because"))
+
+// solution 18
+console.log(soluTion.lastIndexOf("because"))
+
+// solution 19
+console.log(soluTion.search("because"))
+
+// solution 20
+let string = " 30 Days Of JavaScript "  
+console.log(string.trim())
+
+// solution 21
+console.log(challenge.startsWith("30"))
+
+// solution 22
+console.log(challenge.endsWith("Javascript"))
+
+// solution 23
+console.log(challenge.match("a"))
+
+// solution 24
+let a = "30 Days of"
+console.log(a.concat(" JavaScript"))
+
+// solution 25
+console.log(challenge.repeat(2))
+
+// Exercise Level 2
+
+// solution 1
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+
+// solution 2
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
